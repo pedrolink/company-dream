10c9d242ed3580bd2e5895277c68dace50d2d2c3
pedrolink <pedrohplink@gmail.com>
2022-10-30 23:56:20 -0300

    login and create user remaster

--- src/form_create_user.php
+++ src/form_create_user.php
@@ -2,128 +2,216 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+<html class="light-style  customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
+    data-template="vertical-menu-template-free" lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
-    <!-- Link's -->
-    <?php include("./partials/_header.php") ?>
-    <link rel="stylesheet" type="text/css" href="./style/login.css">
-    <title>Company Dream - Cadastro</title>
+    <title>Company Dream - Cadastro de usuário</title>
+
+    <meta name="description"
+        content="Most Powerful &amp; Comprehensive Bootstrap 5 HTML Admin Dashboard Template built for developers!">
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="./style/images/necktie.png">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap"
+        rel="stylesheet">
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css">
+
+
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css">
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="../assets/css/demo.css">
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+
+
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css">
+    <!-- Helpers -->
+    <script src="../assets/vendor/js/helpers.js"></script>
+    <style type="text/css">
+        .layout-menu-fixed .layout-navbar-full .layout-menu,
+        .layout-page {
+            padding-top: 0px !important;
+        }
+
+        .content-wrapper {
+            padding-bottom: 0px !important;
+        }
+    </style>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../assets/js/config.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="async" src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'GA_MEASUREMENT_ID');
+    </script>
+    <!-- Custom notification for demo -->
+    <!-- beautify ignore:end -->
+
 </head>
 
-<body style='background-image: url("./style/images/fundo.jpg")'>
-    <section class="vh-100">
-        <div class="container h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-lg-12 col-xl-11">
-                    
-                    <!-- EXCESSÕES DO FORMULÁRIO -->
-                    <?php include ("./exceptions/error_message_create_user.php")?>
+<body>
 
-                    <div class="card bg-dark text-light" style="border-radius: 25px;">
+    <!-- Content -->
 
-                        <div class="card-body p-md-5">
-                        <?php include("./style/images/arrow.svg") ?>
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <!-- EXCESSÕES DO FORMULÁRIO -->
+                <?php include ("./exceptions/error_message_create_user.php")?>
 
-                            <div class="row justify-content-center">
-                                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                <!-- Register Card -->
+                <div class="card">
+                    <div class="card-body">
+                        <!-- Logo -->
+                        <img width="60" src="./style/images/necktie.png" style="margin-left: 145px" />
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="#" class="app-brand-link gap-2">
+                                <span class="app-brand-text text-body fw-bolder">Company Dream</span>
+                            </a>
+                        </div>
 
-                                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Cadastro</p>
-
-                                    <form action="data_create_user.php" method="POST" class="mx-1 mx-md-4">
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="name">Nome</label>
-                                                <input type="text" name="name" class="form-control" required/>
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-user fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="user">Usuário</label>
-                                                <input type="text" name="user" id="user" class="form-control" required/>
-                                                <div class="valid-feedback">
-                                                    Parece bom!
-                                                </div>
-                                                <div class="invalid-feedback">
-                                                    Por favor, informe outro usuário.
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="email">E-mail</label>
-                                                <input type="email" id="email" name="email" class="form-control" required/>
-                                                <div class="valid-feedback">
-                                                    Parece bom!
-                                                </div>
-                                                <div class="invalid-feedback">
-                                                    Por favor, informe outro e-mail.
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="password">Senha</label>
-                                                <input type="password" id="password" name="password" class="form-control" required/>
-                                                <div class="invalid-feedback">
-                                                    Verifique se as senhas foram preenchidas corretamente.
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <div class="d-flex flex-row align-items-center mb-4">
-                                            <i class="fas fa-key fa-lg me-3 fa-fw"></i>
-                                            <div class="form-outline flex-fill mb-0">
-                                                <label class="form-label" for="confirm_password">Confirme sua
-                                                    senha</label>
-                                                <input type="password" id="confirm_password" name="confirm_password" class="form-control" required/>
-                                            </div>
-                                        </div>
-
-                                        <div class="form-check d-flex justify-content-center mb-5">
-                                            <input class="form-check-input me-2" type="checkbox" value="is_checked"
-                                                name="accept_terms" required/>
-                                            <label class="form-check-label" for="accept_terms">
-                                                Eu li e aceito os <a href="#!">Termos de Serviço</a>
-                                            </label>
-                                        </div>
-
-                                        <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button class="btn btn-outline-light btn-lg px-5"
-                                                type="submit">Registrar</button>
-                                        </div>
-
-                                    </form>
-
+                        <form class="mb-3" action="data_create_user.php" method="POST">
+                            <div class="mb-3">
+                                <label for="name" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="name" name="name"
+                                    placeholder="Digite seu nome" autofocus="" required>
+                            </div>
+                            <div class="mb-3">
+                                <label for="user" class="form-label">Usuário</label>
+                                <input type="text" class="form-control" id="user" name="user"
+                                    placeholder="Digite seu usuário" autofocus="" required>
+                                <div class="valid-feedback">
+                                    Parece bom!
                                 </div>
-                                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">                                    
-                                    <img src="./style/images/create.png" class="img-fluid" alt="Sample image">
-
+                                <div class="invalid-feedback">
+                                    Por favor, informe outro usuário.
                                 </div>
                             </div>
-                        </div>
+                            <div class="mb-3">
+                                <label for="email" class="form-label">E-mail</label>
+                                <input type="email" class="form-control" id="email" name="email"
+                                    placeholder="Digite seu e-mail" required>
+                                <div class="valid-feedback">
+                                    Parece bom!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Por favor, informe outro e-mail.
+                                </div>
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <label class="form-label" for="password">Senha</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="············" aria-describedby="password" required>
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                    <div class="invalid-feedback">
+                                        Verifique se as senhas foram preenchidas corretamente.
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="mb-3 form-confirm_password-toggle">
+                                <label class="form-label" for="confirm_password">Confirme sua senha</label>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="confirm_password" class="form-control"
+                                        name="confirm_password" placeholder="············"
+                                        aria-describedby="confirm_password" required>
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="terms-conditions" name="accept_terms" required>
+                                    <label class="form-check-label" for="terms-conditions">
+                                        Eu li e aceito os
+                                        <a href="javascript:void(0);">termos de serviço</a>
+                                    </label>
+                                </div>
+                            </div>
+                            <button class="btn btn-primary d-grid w-100" type="submit">
+                                Registrar-se
+                            </button>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Você já tem uma conta?</span>
+                            <a href="index.php">
+                                <span>Faça o login aqui</span>
+                            </a>
+                        </p>
                     </div>
                 </div>
+                <!-- Register Card -->
             </div>
         </div>
-    </section>
-</body>
+    </div>
 
-<?php 
-include("./partials/_scripts.php");
-include("./utils/exceptions_scripts/scripts_create_user.php")
-?>
+    <!-- / Content -->
+
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+
+    
+
+
+
+
+</body>
+<?php     
+    include("./utils/exceptions_scripts/scripts_create_user.php")
+    ?>
+
 </html>
--- src/index.php
+++ src/index.php
@@ -2,67 +2,176 @@
 session_start();
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+<html class="light-style  customizer-hide" dir="ltr" data-theme="theme-default" data-assets-path="../assets/"
+    data-template="vertical-menu-template-free" lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0">
 
-    <!-- Link's -->
-    <link rel="stylesheet" type="text/css" href="./style/login.css">
-    <?php include("./partials/_header.php") ?>
+    <title>Company Dream - Login</title>
 
-    <title>Company Dream - Entrar</title>
+    <meta name="description"
+        content="Most Powerful &amp; Comprehensive Bootstrap 5 HTML Admin Dashboard Template built for developers!">
+    <meta name="keywords" content="dashboard, bootstrap 5 dashboard, bootstrap 5 design, bootstrap 5">
+    <!-- Canonical SEO -->
+    <link rel="canonical" href="https://themeselection.com/products/sneat-bootstrap-html-admin-template/">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="./style/images/necktie.png">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap"
+        rel="stylesheet">
+
+    <!-- Icons. Uncomment required icon fonts -->
+    <link rel="stylesheet" href="../assets/vendor/fonts/boxicons.css">
+
+
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="../assets/vendor/css/core.css" class="template-customizer-core-css">
+    <link rel="stylesheet" href="../assets/vendor/css/theme-default.css" class="template-customizer-theme-css">
+    <link rel="stylesheet" href="../assets/css/demo.css">
+
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css">
+
+
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="../assets/vendor/css/pages/page-auth.css">
+    <!-- Helpers -->
+    <script src="../assets/vendor/js/helpers.js"></script>
+    <style type="text/css">
+        .layout-menu-fixed .layout-navbar-full .layout-menu,
+        .layout-page {
+            padding-top: 0px !important;
+        }
+
+        .content-wrapper {
+            padding-bottom: 0px !important;
+        }
+    </style>
+
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="../assets/js/config.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async="async" src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+        gtag('config', 'GA_MEASUREMENT_ID');
+    </script>
+    <!-- Custom notification for demo -->
+    <!-- beautify ignore:end -->
+
 </head>
 
-<body style='background-image: url("./style/images/fundo.jpg")'>
-    <section class="vh-100 gradient-custom">
-        <div class="container py-5 h-100">
-            <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                    <?php include("./exceptions/error_message_login_user.php") ?>
-                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
-                        <div class="card-body p-5 text-center">
-                            <img src="./style/images/necktie.png" class="login-image" alt="Necktie" />
-                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+<body>
 
-                            <div class="mb-md-5 mt-md-4 pb-5">
-                                <p class="text-white-50 mb-5">Por favor entre com seu usuário e senha!</p>
-                                <form action="data_login.php" method="POST">
-                                    <div class="form-outline form-white mb-4">
-                                        <label class="form-label d-flex justify-content-left"
-                                            for="username">Usuário</label>
-                                        <input type="test" name="user" class="form-control form-control-lg" />
-                                    </div>
+    <!-- Content -->
 
-                                    <div class="form-outline form-white mb-4">
-                                        <label class="form-label d-flex justify-content-left"
-                                            for="password">Senha</label>
-                                        <input type="password" name="password" class="form-control form-control-lg" />
-                                    </div>
-
-                                    <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Esqueceu sua
-                                            senha?</a>
-                                    </p>
-
-                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">Entrar</button>
-                                </form>
-                            </div>
-
-                            <div>
-                                <p class="mb-0">Não tem uma conta? <a href="form_create_user.php"
-                                        class="text-white-50 fw-bold">Cadastrar-se</a>
-                                </p>
-                            </div>
-
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner">
+                <?php include("./exceptions/error_message_login_user.php") ?>
+                <!-- Register -->
+                <div class="card">
+                    <div class="card-body">
+                        <img width="60" src="./style/images/necktie.png" style="margin-left: 145px" />
+                        <!-- Logo -->
+                        <div class="app-brand justify-content-center">
+                            <a href="#" class="app-brand-link gap-2">
+                                <span class="app-brand-text text-body fw-bolder">Company Dream</span>
+                            </a>
                         </div>
+
+                        <form class="mb-3" action="data_login.php" method="POST">
+                            <div class="mb-3">
+                                <label for="user" class="form-label">Usuário</label>
+                                <input type="text" class="form-control" id="user" name="user"
+                                    placeholder="Entre com seu usuário" autofocus="">
+                            </div>
+                            <div class="mb-3 form-password-toggle">
+                                <div class="d-flex justify-content-between">
+                                    <label class="form-label" for="password">Senha</label>
+                                </div>
+                                <div class="input-group input-group-merge">
+                                    <input type="password" id="password" class="form-control" name="password"
+                                        placeholder="············" aria-describedby="password">
+                                    <span class="input-group-text cursor-pointer"><i class="bx bx-hide"></i></span>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="remember-me">
+                                    <label class="form-check-label" for="remember-me">
+                                        Lembrar senha
+                                    </label>
+                                    <a href="auth-forgot-password-basic.html" style="margin-left: 100px">
+                                        <small>Esqueceu a senha?</small>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="mb-3">
+                                <button class="btn btn-primary d-grid w-100" type="submit">Entrar</button>
+                            </div>
+                        </form>
+
+                        <p class="text-center">
+                            <span>Novo na plataforma?</span>
+                            <a href="form_create_user.php">
+                                <span>Crie sua conta aqui</span>
+                            </a>
+                        </p>
                     </div>
                 </div>
+                <!-- /Register -->
             </div>
         </div>
-    </section>
+    </div>
+
+    <!-- / Content -->
+
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+    <script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../assets/vendor/libs/popper/popper.js"></script>
+    <script src="../assets/vendor/js/bootstrap.js"></script>
+    <script src="../assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../assets/vendor/js/menu.js"></script>
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+
+
+
+    <!-- Main JS -->
+    <script src="../assets/js/main.js"></script>
+
+    <!-- Page JS -->
+
+
+
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
+
+
+
+
 </body>
-<?php include("./partials/_scripts.php") ?>
+
 </html>
